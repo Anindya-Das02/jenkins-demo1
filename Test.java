6822c94ba99abe89a1e5ae7fcf73ec8551b0ed5b
anindya-mint <anindyadasofficial@gmail.com>
2023-03-26 00:18:43 +0530

    dev changes

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test{
 	public static void main(String[] args){
-		System.out.println("Jenkin auto git trigger test");
+		System.out.println("[OUT] Jenkin auto git trigger test");
+		System.out.println("[OUT] new changes");
 	}
 }
 
