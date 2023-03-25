b4c28271a8902c5a8f423a6a5e6778e85671c29c
anindya-mint <anindyadasofficial@gmail.com>
2023-03-26 00:25:16 +0530

    dev2 branch

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
 	public static void main(String[] args){
 		System.out.println("[OUT] Jenkin auto git trigger test");
-		System.out.println("[OUT] new changes");
+		System.out.println("[OUT] new changes .. missing token ;")		
 	}
 }
 
